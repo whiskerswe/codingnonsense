7512805b807a780a6502a59e0f255df164280f49
whiskerswe <sara.wanerskar@gmail.com>
2026-05-05 18:52:24 +0200

    Move empty css file to build folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./styles/main.css";
+import "./app/main.css";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
